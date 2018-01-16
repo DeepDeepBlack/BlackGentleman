1ec3d744d2846c978f53771be67144fc84c726e7
DeepDeepBlack <DeepDeepBlack@protonmail.com>
2018-01-16 17:32:54 +0000

    TemperatureConversion

---
+++ C/TemperatureConversion/CtoF.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int	main()
+{
+	float cdegree = 100.0;
+	float fdegree = (9.0 / 5.0) * cdegree + 32.0;
+
+	printf("%.1f\n", fdegree);
+
+	return (0);
+}
---
+++ C/TemperatureConversion/FtoC.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int	main()
+{
+	float fdegree = 212.0;
+	float cdegree = 5.0 / 9.0 * (fdegree - 32.0);
+
+	printf("%.1f\n", cdegree);
+
+	return (0);
+}
