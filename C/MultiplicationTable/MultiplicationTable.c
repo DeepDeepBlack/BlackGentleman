86992f0ca998f4f03ad59bd97e36f4a395c4f299
DeepDeepBlack <DeepDeepBlack@protonmail.com>
2018-01-16 18:36:44 +0000

    C practice

---
+++ C/MultiplicationTable/MultiplicationTable.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int	main()
+{
+	int	i, j;
+
+	for(i = 1; i < 10; i++)
+	{
+		for(j = 1; j < 10; j++)
+		{
+			printf(" %2d * %2d = %2d", i, j, i * j);
+		}
+		printf("\n");
+	}
+
+	return (0);
+}
