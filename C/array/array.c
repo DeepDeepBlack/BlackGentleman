a279e06b242d36c67dd920d18a1608c5e36e0ab0
DeepDeepBlack <DeepDeepBlack@protonmail.com>
2018-01-25 01:48:08 +0800

    C practice

---
+++ C/array/array.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int	main(void)
+{
+	int	array[10];
+
+	array[0] = 100;
+	array[1] = 90;
+	array[6] = 12;
+
+	printf("%d %d %d\n", array[0], array[1], array[6]);
+
+	return (0);
+}
