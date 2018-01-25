9bbdbcabde595110b3a46d733897ac7de970eb0f
DeepDeepBlack <DeepDeepBlack@protonmail.com>
2018-01-25 19:16:37 +0800

    C practice

---
+++ C/array/PrintString.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int	main(void)
+{
+	char	name[17];
+
+	name[0] = 'N';
+	name[1] = 'A';
+	name[2] = 'I';
+	name[3] = 'N';
+	name[4] = 'T';
+	name[5] = 'E';
+	name[6] = 'I';
+	name[7] = 'R';
+	name[8] = 'U';
+	name[9] = 'K';
+	name[10] = 'I';
+	name[11] = 'T';
+	name[12] = 'S';
+	name[13] = 'U';
+	name[14] = 'N';
+	name[15] = 'E';
+	name[16] = '\0';
+
+	printf("%s\n", name);
+
+	return(0);
+}
