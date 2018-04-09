96f4d07fc38cb291cfb004842b474164ba11d425
DeepDeepBlack <DeepDeepBlack@protonmail.com>
2018-04-09 11:43:48 +0800

    preprocessor

---
+++ C/preprocessor/preprocessor1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#define     CPU     "Central Processing Unit"
+#define     PI      3.141590
+
+int     main(void)
+{
+    printf("CPU is short for %s\n", CPU);
+    printf("π = %f\n", PI);
+
+    return (0);
+}
